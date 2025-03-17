e6c6ed799808112d620d05d271fd1f09adbc54c1
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2025-03-17 16:58:18 +0100

    why in hell would u do that ??? fixed not updating custom role on changingrole

--- EXILED/Exiled.CustomRoles/API/Features/CustomRole.cs
+++ EXILED/Exiled.CustomRoles/API/Features/CustomRole.cs
@@ -926,13 +926,8 @@ namespace Exiled.CustomRoles.API.Features
 
         private void OnInternalChangingRole(ChangingRoleEventArgs ev)
         {
-            if(ev.Reason == SpawnReason.Destroyed)
-                return;
-
-            if (Check(ev.Player) && ((ev.NewRole == RoleTypeId.Spectator && !KeepRoleOnDeath) || (ev.NewRole != RoleTypeId.Spectator && ev.NewRole != Role && !KeepRoleOnChangingRole)))
-            {
+            if (ev.IsAllowed && ev.Reason != SpawnReason.Destroyed && Check(ev.Player) && ((ev.NewRole == RoleTypeId.Spectator && !KeepRoleOnDeath) || (ev.NewRole != RoleTypeId.Spectator && !KeepRoleOnChangingRole)))
                 RemoveRole(ev.Player);
-            }
         }
 
         private void OnSpawningRagdoll(SpawningRagdollEventArgs ev)
