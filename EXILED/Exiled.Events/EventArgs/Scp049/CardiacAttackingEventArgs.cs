0fd6d0cde12f55f8c92d33768164dbd8c79781c6
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2025-05-20 13:54:03 +0900

    Useless patch for re-examination 2

--- EXILED/Exiled.Events/EventArgs/Scp049/CardiacAttackingEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Scp049/CardiacAttackingEventArgs.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="CardiacAttacking안녕하세요EventArgs.cs" company="ExMod Team">
+// <copyright file="CardiacAttackingEventArgs.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
