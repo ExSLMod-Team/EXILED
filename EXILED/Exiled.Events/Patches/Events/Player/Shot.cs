8799715a35455ddfdd9cd3b3b6fa064a1cf1a9fb
Banalny-Banan <elisey1324@gmail.com>
2025-05-23 01:43:47 +0300

    😓

--- EXILED/Exiled.Events/Patches/Events/Player/Shot.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/Shot.cs
@@ -7,16 +7,14 @@
 
 namespace Exiled.Events.Patches.Events.Player
 {
-    using System.Collections.Generic;
-    using System.Reflection.Emit;
+    using System;
 
-    using Attributes;
+    using Exiled.API.Features;
+    using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
     using HarmonyLib;
     using InventorySystem.Items.Firearms.Modules;
     using InventorySystem.Items.Firearms.Modules.Misc;
-
-    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="HitscanHitregModuleBase.ServerApplyDamage" />.
@@ -27,72 +25,35 @@ namespace Exiled.Events.Patches.Events.Player
     internal static class Shot
     {
         // public virtual void ServerApplyDamage(HitscanResult result)
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+#pragma warning disable SA1313
+        private static bool Prefix(HitscanResult result, HitscanHitregModuleBase __instance)
+#pragma warning restore SA1313
         {
-            CodeMatcher matcher = new(instructions);
-            matcher.Start();
+            ShotEventArgs args = new(result, __instance);
+            Handlers.Player.OnShot(args);
 
-            LocalBuilder eventArgs = il.DeclareLocal(typeof(ShotEventArgs));
-
-            /* Insert event call
-            ShotEventArgs args = new ShotEventArgs(result, this);
-            Handlers.Player.OnShot(args);*/
-            matcher.Insert(new List<CodeInstruction>
+            try
             {
-                // ShotEventArgs args = new ShotEventArgs(result, this);
-                new(OpCodes.Ldarg_1), // result
-                new(OpCodes.Ldarg_0), // this
-                new(OpCodes.Newobj, Constructor(typeof(ShotEventArgs), new[] { typeof(HitscanResult), typeof(HitscanHitregModuleBase) })),
-                new(OpCodes.Stloc, eventArgs),
+                if (args.CanDamageDestructibles)
+                {
+                    foreach (DestructibleHitPair destructible in result.Destructibles)
+                        __instance.ServerApplyDestructibleDamage(destructible, result);
+                }
 
-                // Handlers.Player.OnShot(args);
-                new(OpCodes.Ldloc, eventArgs),
-                new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnShot))),
-            }).Advance(6);
+                if (args.CanDamageObstacles)
+                {
+                    foreach (HitRayPair obstacle in result.Obstacles)
+                        __instance.ServerApplyObstacleDamage(obstacle, result);
+                }
 
-            /* Insert the if's:
-
-            if (args.CanDamageDestructibles)
+                __instance.ServerSendAllIndicators(result);
+            }
+            catch (Exception e)
             {
-                foreach (DestructibleHitPair destructible in result.Destructibles)
-                    __instance.ServerApplyDestructibleDamage(destructible, result);
+                Log.Error($"Error in {nameof(Shot)} event - invalud modification of ev.{nameof(ShotEventArgs.ShotResult)} has caused an exception: {e}");
             }
 
-            if (args.CanDamageObstacles)
-            {
-                foreach (HitRayPair obstacle in result.Obstacles)
-                    __instance.ServerApplyObstacleDamage(obstacle, result);
-            }*/
-
-            bool LoadsShotResult(CodeInstruction i) => i.IsLdarg(1);
-
-            // Labels for skipping loops
-            Label skipDestructiblesLabel = il.DefineLabel();
-            Label skipObstaclesLabel = il.DefineLabel();
-
-            matcher
-                .SearchForward(LoadsShotResult)
-                .Insert(new List<CodeInstruction>
-                {
-                    new(OpCodes.Ldloc, eventArgs),
-                    new(OpCodes.Callvirt, PropertyGetter(typeof(ShotEventArgs), nameof(ShotEventArgs.CanDamageDestructibles))),
-                    new(OpCodes.Brfalse, skipDestructiblesLabel),
-                })
-                .SearchForward(LoadsShotResult).Advance(1) // Skip result inside of foreach
-
-                .SearchForward(LoadsShotResult)
-                .Insert(new List<CodeInstruction>
-                {
-                    new(OpCodes.Ldloc, eventArgs),
-                    new(OpCodes.Callvirt, PropertyGetter(typeof(ShotEventArgs), nameof(ShotEventArgs.CanDamageObstacles))),
-                    new(OpCodes.Brfalse, skipObstaclesLabel),
-                }).AddLabels(new[] { skipDestructiblesLabel })
-                .SearchForward(LoadsShotResult).Advance(1) // Skip result inside of foreach
-
-                .SearchForward(i => i.IsLdarg(0)) // Go before the last call
-                .AddLabels(new[] { skipObstaclesLabel });
-
-            return matcher.Instructions();
+            return false;
         }
     }
 }
